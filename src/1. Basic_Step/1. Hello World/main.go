4379bf2a74b2e6743667d3cec97c74a18d38f4ed
4.N02mE <sino2me@naver.com>
2022-02-22 17:25:05 +0900

    Update Basic Step 1

--- src/1. Basic_Step/1. Hello World/main.go
+++ src/1. Basic_Step/1. Hello World/main.go
@@ -1,7 +1,9 @@
 package main
 
-// package main -> Entry Point(진입점)
-//package main > func main 순으로 찾음
+/*
+package main -> Entry Point (진입점)
+코드 실행 시 package main -> func main() 순서로 찾아내어 실행한다.
+*/
 
 import (
 	"fmt"
@@ -9,4 +11,10 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
+	/*
+		fmt -> 기본적으로 제공하는 패키지
+		fmt.Println(): stdout으로 표준 문자열 출력을 제공하는 함수
+		println(): 내장된 함수로 stderr를 출력해주는 내장함수
+		           때문에 주로 디버깅할 때에 유용
+	*/
 }
