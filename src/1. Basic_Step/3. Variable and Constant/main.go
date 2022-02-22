f5d62f1e620714a406e3837ace8d5d433294cee9
4.N02mE <sino2me@naver.com>
2022-02-22 17:54:58 +0900

    Update Basic Step 3

--- src/1. Basic_Step/3. Variable and Constant/main.go
+++ src/1. Basic_Step/3. Variable and Constant/main.go
@@ -6,43 +6,49 @@ import (
 
 func main() {
 	/*
-		자료형 및 변수
-		Go -> 정적 타입 언어
-		: 컴파일 타임에 타입이 결정되며, 런타임 중 동적으로 타입 변경 X
-		  런타임 중 타입 추론 및 최적화 진행 상황 X -> 대체로 빠른 속도
-		Go는 사용하지 않는 변수가 있는 경우 에러 표시
+		Go는 정적 타입 언어이다.
+		컴파일 타임에 타입이 결정되며, 런타임 중 동적으로 타입변경이 불가하다.
+		하지만 런타임 중 타입 추론 및 최적화 진행 상황이 없어
+		대체로 빠른 속도를 갖는다.
+		또한 Go는 사용하지 않는 변수가 있을 시 에러가 표시된다.
 
-		변수 선언 방법:
-		1. var {name} {type} -> {name} = {value}
-		Ex)	var example int
-			example = 0
+		변수/상수는 키워드 "var"/"const" 를 사용하여 선언한다.
+		"var"/"const" 키워드 뒤에 변수의 이름과 타입을 기제하여 선언한다.
+		Ex) var x int | const y string
 
-		2. var {name} {type} = {value}
-		Ex) var example int = 0
+		변수 선언 문법 4가지
+		var number_1 int 		-> 변수 선언 후 값 초기화
+		num_1 = 0
 
-		3. var {name} = {value}
-		Ex) var example = 0
+		var num_2 int = 0	-> 변수 선언과 동시에 초기화
 
-		4. {name} := {value} (변수 단축 선언 방법)
-		Ex) example := {value}
+		var num_3 = 0		-> 변수 타입을 생략하여 선언
 
-		* 만약 타입이 명시되지 않은 경우 컴파일 시 자동으로 타입을 추론 *
+		num_4 := 0			-> "var" 키워드와 변수 타입을 생략하여 선언
+		[!]	변수 타입이 명시되지 않은 경우 컴파일 시 자동으로 타입을
+			추론하여 타입이 붙게 된다.
+
+		상수 선언 문법
+		const num_1 int = 0
+		[!]	상수 선언 시 주의점
+			1. 한 번 선언되고 할당되면 값을 바꿀 수 없다.
+			2. 선언과 초기화를 따로 할 수 없으며 선언할 때 초기화를
+			   같이 해주어야 한다.
 	*/
+	// 첫 번째 선언 방법
+	var number_1 int
+	number_1 = 1
 
-	//int
-	var num_1 int = 1
-	num_2 := 2
-	fmt.Println("num_1: ", num_1, "num_2:", num_2)
+	// 두 번쨰 선언 방법
+	var number_2 int = 2
 
-	//string
-	var str_1 string = "str_variable 1"
-	str_2 := "str_variable 2" // string
-	fmt.Println("str_1:", str_1, "str_2:", str_2)
+	// 세 번째 선언 방법
+	var number_3 = 3
 
-	//boolean
-	var bool_1 bool = true
-	var bool_2 bool = false
-	fmt.Println("bool_1:", bool_1, "bool_2:", bool_2)
+	// 네 번째 선언 방법
+	number_4 := 4
+
+	fmt.Println(number_1, number_2, number_3, number_4)
 
 	/*
 		여러 개의 변수 방법:
@@ -60,16 +66,6 @@ func main() {
 		반드시 선언하는 변수와 초기화하는 값의 개수가 같아야 하며,
 		타입은 똑같지 않아도 된다bool_1
 	*/
-
-	num_3, str_3, bool_3 := 250, "str_variable 3", false
-	fmt.Println("num_3:", num_3, "str_3:", str_3, "bool_3:", bool_3)
-
-	var (
-		num_4  int
-		str_4  string
-		bool_4 bool
-	)
-	fmt.Println("num_4:", num_4, "str_4:", str_4, "bool_4:", bool_4)
 
 	/*
 		상수 선언 방법
@@ -91,15 +87,6 @@ func main() {
 		1. 한 번 선언되고 할당되면 값을 바꿀 수 없다.
 		2. 선언과 할당은 따로 할 수 없으며 선언할 떄 초기화를 같이 해주어야 한다.
 	*/
-	num_5, str_5, bool_5 := 256, "str_variable 5", true
-	fmt.Println("num_5:", num_5, "str_5:", str_5, "bool_5:", bool_5)
-
-	var (
-		num_6  int    = 512
-		str_6  string = "str_variable 6"
-		bool_6 bool   = true
-	)
-	fmt.Println("num_6:", num_6, "str_6:", str_6, "bool_6:", bool_6)
 }
 
 /*
